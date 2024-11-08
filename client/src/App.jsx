d7ad8b1fc28035c925b202b204dfb5c7447428f2
+ <+>
2024-11-08 17:56:52 +0300

    Deleted components folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EmployerLandingPage from './pages/EmployerLandingPage';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<EmployerLandingPage />} />
-        {/* Add other routes as needed */}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<EmployerLandingPage />} />
+                {/* Add other routes as needed */}
+                
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
