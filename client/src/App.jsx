dd3367e411639a83df653aecb23a722d92197e81
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-11-09 12:09:55 +0300

    Implementing Payment page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EmployerLandingPage from './pages/EmployerLandingPage';
+import PaymentPage from './pages/PaymentPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/ELandingPage" element={<EmployerLandingPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
         {/* Add other routes as needed */}
       </Routes>
     </Router>
