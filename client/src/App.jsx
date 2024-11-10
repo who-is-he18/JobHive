46ddb783f33dca7875b1a123e0c2b5280ed91227
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-11-10 13:24:45 +0300

    Fixing App.jsx and main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,16 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import EmployerLandingPage from './pages/EmployerLandingPage';
 import PaymentPage from './pages/PaymentPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/ELandingPage" element={<EmployerLandingPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        {/* Add other routes as needed */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/ELandingPage" element={<EmployerLandingPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
+      {/* Add other routes as needed */}
+    </Routes>
   );
 }
 
