e8d464dc8dda1658d0e742222b6601dc731eef59
amos <amosmokua12@gmail.com>
2024-11-11 11:15:10 +0300

    Committing changes from master

---
+++ src/components/JobseekerProfile/DocumentsSection.js
@@ -0,0 +1,16 @@
+// src/components/JobseekerProfile/DocumentsSection.js
+import React from 'react';
+
+const DocumentsSection = () => {
+  return (
+    <div className="documents-section">
+      <h3>Documents</h3>
+      <ul>
+        <li><a href="#">Resume.pdf</a></li>
+        <li><a href="#">CoverLetter.pdf</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default DocumentsSection;
