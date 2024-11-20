0949aa1be2a1928ff5b412ed9975fd419c8c54b5
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-11-20 22:19:21 +0300

    Added vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // This should be 'dist'
+    outDir: 'build', // This should be 'dist'
   },
   esbuild: {
     jsx: 'automatic',
